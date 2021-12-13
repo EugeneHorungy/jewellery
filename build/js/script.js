e63569934e81f2d9bd5231ea822d5f7c2dafa753
EugeneHorungy <77688070+EugeneHorungy@users.noreply.github.com>
2021-12-14 00:51:25 +0300

    adds slider and connects swiperjs

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,4 @@
 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
 
 const page = document.querySelector('.page-body');
 const header = document.querySelector('.header');
@@ -23,6 +9,21 @@ const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu-button');
 const logo = document.querySelector('.logo');
 const mobileCart = document.querySelector('.mobile-cart__icon');
+const slider = document.querySelector('.swiper');
+const noveltyCards = document.querySelectorAll('.novelty-card');
+const noveltiesControls = document.querySelectorAll('.novelties__controls');
+
+if (noveltyCards) {
+  noveltyCards.forEach((card) => {
+    card.classList.remove('novelty-card--no-js');
+  })
+}
+
+if (noveltiesControls) {
+  noveltiesControls.forEach((btn) => {
+    btn.classList.remove('novelties__controls--no-js');
+  })
+}
 
 header.classList.remove('header--no-js');
 logo.classList.remove('logo--no-js');
@@ -40,10 +41,6 @@ const switchMenu = () => {
   logo.classList.toggle('logo--menu-open');
   page.classList.toggle('page-body--menu-open');
   infoList.classList.toggle('info-list--js');
-  // navButton.classList.toggle('nav__button--closed');
-  // navButton.classList.toggle('nav__button--opened');
-  // nav.classList.toggle('nav--menu-open');
-  // page.classList.toggle('page-body--menu-open');
 };
 
 menuBtn.addEventListener('click', (evt) => {
@@ -51,4 +48,75 @@ menuBtn.addEventListener('click', (evt) => {
   switchMenu();
 });
 
-console.log('hello');
+// SWIPER
+
+if (slider) {
+  const initSwiper = () => {
+    new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: false,
+      observer: true,
+      observeParents: true,
+      resizeReInit: true,
+      spaceBetween: 30,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            renderFraction: function (currentClass, totalClass) {
+              return `<span class= ${currentClass}></span> <span>of</span> <span class= ${totalClass}></span>`;
+            },
+          },
+        },
+
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            renderBullet: function (index, className) {
+              return `<button class="${className}">${index + 1}</button>`;
+            },
+            clickable: true,
+          },
+        },
+
+        1024: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            renderBullet: function (index, className) {
+              return `<button class="${className}">${index + 1}</button>`;
+            },
+            clickable: true,
+          },
+        }
+      }
+    });
+  }
+
+  initSwiper();
+
+  window.addEventListener('resize', () => {
+    const swiperPagination = document.querySelector('.swiper-pagination');
+    if (window.innerWidth < 768 && swiperPagination.firstElementChild.nodeName === 'BUTTON') {
+      console.log(`Screen width: ${window.innerWidth}`);
+      initSwiper();
+    } else if (window.innerWidth > 767 && swiperPagination.firstElementChild.nodeName === 'SPAN') {
+      console.log(`Screen width: ${window.innerWidth}`);
+      initSwiper();
+    }
+  });
+};
